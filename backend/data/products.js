07f6788c1858df00af837701ca4caf91455488e8
Risa <L8601@student.jamk.fi>
2020-10-18 14:27:01 +0300

    final version yumyum

---
+++ backend/data/products.js
@@ -0,0 +1,100 @@
+const products = [
+  {
+    name: 'LOOK: Sweet Potato & Chestnut',
+    image: '/images/sweetpotato.jpg',
+    description:
+      'This box of fall-flavored chocolate features chestnut and sweet potato, both Japanese classics.',
+    brand: 'LOOK',
+    category: 'Chocolate',
+    price: 2.56,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Frozen Coca Cola Lemon',
+    image: '/images/coke.jpg',
+    description:
+      'Frozen Coke?! This frosty treat is back! Let it freeze, squeeze the pack and enjoy a frosty Coke! Easy to hold, this coke with a fruity lemon twist is perfect anywhere! Or combine it with a coke for a chilly lemon twist!',
+    brand: 'Coca Cola',
+    category: 'Drinks',
+    price: 4.25,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Pocky Chocolate Halloween Party Pack',
+    image: '/images/chocolatepocky.jpg',
+    description:
+      'No Halloween party is complete without Pocky! Score your Halloween exclusive chocolate Pocky - only around for a limited time!',
+    brand: 'Pocky',
+    category: 'Chocolate',
+    price: 7.09,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Kit Kat Mini Frozen Cookies and Cream',
+    image: '/images/kitkatcookies.jpg',
+    description:
+      'Wow! These unique Kit Kat chocolates are a cookies and cream lovers dream! These are such a delicious and unique Kit Kat flavor that you just HAVE to try!',
+    brand: 'Kitkat',
+    category: 'Chocolate',
+    price: 5.77,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Giant Pocky Uji Matcha',
+    image: '/images/kitkatmatcha.jpg',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Pocky',
+    category: 'Chocolate',
+    price: 20.63,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Cup Noodle Tom Yum Goong Instant Ramen',
+    image: '/images/cupnoodle.png',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'NIssin',
+    category: 'Ramen',
+    price: 4.62,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'KitKat Sea Salt Mini (12pcs)',
+    image: '/images/kitkatsalt.jpg',
+    description:
+      'These NEW KitKats are the perfect combination of Japanese setouchi sea salt and white chocolate. The usual plastic packaging has been replaced by a new eco-friendly paper packaging and the best thing? For every purchase KitKat donates 10yen to the Zero Waste Activities. A must-try for every friend of the ocean and chocolate!',
+    brand: 'Kitkat',
+    category: 'Chocolate',
+    price: 5.87,
+    countInStock: 2,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Kagome - Vegetables Curry',
+    image: '/images/curry.jpg',
+    description:
+      'Enjoy the convenience of a quick reheatable meal with the taste of a restaurent quality dish! This doria features a yummy vegetable curry sauce that will leave your taste buds tingling!',
+    brand: 'Kagome',
+    category: 'Other',
+    price: 6.35,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  }
+]
+
+export default products
